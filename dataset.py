1105e2f06c70140a561c53079131b221a75a8376
Hypurl <jeff060812@gmail.com>
2026-03-25 13:08:46 -0400

    added so that main shows first mri scan

--- dataset.py
+++ dataset.py
@@ -10,6 +10,7 @@ import pandas as pd
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
 
 class BreastDCEDataset(Dataset):
     def __init__(self, csv_dir, img_dir, training_set = True):
@@ -44,7 +45,7 @@ class BreastDCEDataset(Dataset):
         img_tensor = torch.from_numpy(nib_img)
         label_tensor = torch.tensor(label, dtype=torch.float32)
         
-        img_tensor = torch.nn.functional.interpolate(img_tensor, size=(256, 256, 32), mode='trilinear', align_corners=False)
+        img_tensor = torch.nn.functional.interpolate(img_tensor.unsqueeze(0), size=(256, 256, 32), mode='trilinear', align_corners=False).squeeze(0)
         
         # TODO: Normalize voxel values
         
@@ -55,6 +56,18 @@ if __name__ == "__main__":
     CSVPATH = "./data/BreastDCEDL_metadata_min_crop.csv"
     IMGPATH = "./data/BreastDCEDL_ISPY1_min_crop"
     
-    dataset = BreastDCEDataset(csv_dir=CSVPATH, img_dir=IMGPATH, training_set=True)
+    dataset = BreastDCEDataset(csv_dir=CSVPATH, img_dir=IMGPATH, training_set=False)
 
     print(f"{len(dataset)} MRI scans found.")
+    
+    # Show first mri photo
+    img = dataset[0][0].squeeze(0).numpy()
+
+    fig, axes = plt.subplots(1, 3, figsize=(9, 3))
+    
+    for i, s in enumerate([0, 15, 31]):
+        axes[i].imshow(img[:, :, s])
+        axes[i].set_title(f"Slice {s+1}")
+        axes[i].axis("off")
+        
+    plt.show()
