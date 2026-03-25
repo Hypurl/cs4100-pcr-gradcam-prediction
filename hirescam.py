734e54ecc3aae231fa0d5237870d64951d898b87
Peter SantaLucia <pdsl2005@gmail.com>
2026-03-25 17:21:08 -0400

    Add Sphinx documentation structure and update requirements

--- hirescam.py
+++ hirescam.py
@@ -1,6 +1,12 @@
 """
 Inference layer using https://github.com/rachellea/hirescam
+
+Context: CNNs compute gradients :math:`x=asdf`
+Methodology: HiResCAM uses weighted gradients computed by the CNN
+
+
 """
 
 class HiResCam():
     def __init__(self):
+        pass
